7ac6dc0bd0e0ef4c112639eccf0f01023361ee88
806gw <shjea4364@naver.com>
2025-12-19 20:41:15 +0900

    initial commit

---
+++ C/ep1.c
@@ -0,0 +1,14 @@
+// C언어 본격 맛보기
+
+#include <stdio.h> // STanDard Input Output (표준 입출력)
+
+int main() { // 실제 프로그램이 실행되는 곳
+    printf("Hello, World!\n"); // 출력문
+    return 0; // 반환문
+}
+
+/*
+ 리턴값이 0이여야 하는 이유:
+ 컴퓨터에게 무사히 종료되었다는 것을 알림
+ 1은 보통 오류가 발생했다는 것을 알림
+*/
